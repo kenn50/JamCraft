44d8cf64fbf8bc1ee10b7b6f4a0c83218b3edfdf
dbasjmn <antonhvidt@gmail.com>
2025-03-08 11:33:46 +0100

    Stuf

--- JamCraft_Unity_2025/Assets/Bullet.cs
+++ JamCraft_Unity_2025/Assets/Bullet.cs
@@ -11,11 +11,34 @@ public class Bullet : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        Destroy(gameObject, 10);
         rb = GetComponent<Rigidbody2D>();
         //target = ((Vector2)(Camera.main.ScreenToWorldPoint(Input.mousePosition)-transform.position) + Random.insideUnitCircle * randomness);
         rb.linearVelocity = ((Vector2)(Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position)).normalized*speed;
         //transform.position =  Vector2.MoveTowards(transform.position, ((Vector2)(Camera.main.ScreenToWorldPoint(Input.mousePosition)*100) + Random.insideUnitCircle * randomness), speed);
         transform.Rotate(0, 0, Mathf.Atan2(rb.linearVelocity.normalized.y, rb.linearVelocity.normalized.x) * 180 / Mathf.PI - 90);
     }
+
+
+    // This method is called when the bullet collides with another object
+    private void OnCollisionEnter2D(Collision2D collision) {
+        // Check if the object we collided with has the "Meteor" tag
+        if (collision.gameObject.CompareTag("Meteor")) {
+            // Find the Meteor script on the collided object
+            Meteor meteorScript = collision.gameObject.GetComponent<Meteor>();
+
+            // If the Meteor script is found, call its Explode function
+            if (meteorScript != null) {
+                meteorScript.Explode();
+            }
+
+            // Optionally, destroy the bullet after collision
+            Destroy(gameObject);
+        }
+    }
+
+
+
+
 
 }
